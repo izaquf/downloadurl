042fe5a53961a9bdfbf0d0f74fc238f2d6204553
izaquf <izaquf@gmail.com>
2026-05-05 22:42:37 -0300

    Download files via URL

--- downloadurl.py
+++ downloadurl.py
@@ -253,9 +253,9 @@ Commands:
 	-swf          View the list of files before downloading
 
 Examples:
-	python downloadurl.py https://example.com/files/
-	python downloadurl.py https://example.com/ -t zip rar -o myFile
-	python downloadurl.py https://example.com/ -swf
+	python3 downloadurl.py https://example.com/files/
+	python3 downloadurl.py https://example.com/ -t zip rar -o myFile
+	python3 downloadurl.py https://example.com/ -swf
 	""")
 
 def main():
